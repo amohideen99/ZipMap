0cef60e9d0e99d24a82299765ad4e44a653325bd
Ahkeel Mohideen/Desktop <amohideen99@gmail.com>
2018-06-02 19:32:41 -0700

    Accuracy pretty good at center of map - decreases with readius.

--- src/ZipGPS.java
+++ src/ZipGPS.java
@@ -1,41 +1,91 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class ZipGPS {
 
-    double DpxDN = 903 / (50 - 21);
-    double DpxDW = 1205 / (130 - 63);
+    double DpxDN = (double) (834) / (50 - 21);
+    double DpxDW = (double) (1116) / (127 - 65);
 
 
-    public ZipGPS() {
+    public ZipGPS() throws IOException {
+
+        initGUI();
+
     }
 
-    ;
 
-    public double deltaX(int heightPX) {
+    public double deltaX(double heightPX) {
 
-        return (-0.0638279 * heightPX);
+        return (2.96268 * Math.pow(10, -8) * Math.pow(heightPX, 3) - 0.0000352381 * Math.pow(heightPX, 2) - 0.0533567 * heightPX + 3.22582);
     }
 
-    public double deltaY(int widthPX) {
+    public double deltaY(double widthPX) {
 
-        double y = -0.000324907 * Math.pow(widthPX, 2) + 0.382076 * widthPX;
+        double y = -1.49721 * Math.pow(10, -7) * Math.pow(widthPX, 3) - 0.000324907 * Math.pow(widthPX, 2) + 0.382076 * widthPX - 21.188;
 
         return y;
     }
 
     public Point convert(double latitude, double longitude) {
 
-        double width = DpxDN * (latitude - 21) + deltaX((int) (DpxDN * (latitude - 21)));
-        double height = DpxDW * (longitude - 67) + deltaX((int) (DpxDW * (longitude - 67)));
+        double ycord = 56 + (DpxDN * (50 - latitude));
+        double xcord = 48 + (DpxDW * (128.3 - longitude));
 
-        return new Point((int) width, (int) height);
+        double ycordFirst = ycord + deltaY(xcord);
+        double xcordFirst = xcord + deltaX(ycordFirst);
+        ycord += deltaY(xcordFirst);
+        xcord += deltaX(ycord);
+
+        return new Point((int) xcord, (int) ycord);
     }
+
+    public void initGUI() throws IOException {
+
+        JFrame frame = new JFrame("ZipGPS");
+
+        File file = new File("src/utm-USA.jpg");
+
+
+          final BufferedImage Image = ImageIO.read(file);
+
+
+        JPanel panel = new JPanel(){
+
+            @Override
+            public void paint(Graphics g) {
+                super.paint(g);
+                g.drawImage(Image,0,0,null);
+                g.setColor(Color.BLACK);
+
+                int XCORD = (int) convert(35,120).getX();
+                int YCORD = (int) convert(35,120).getY();
+                g.fillOval(XCORD, YCORD, 5, 5);
+            }
+        };
+
+        frame.add(panel);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+     //   frame.setMaximumSize(new Dimension(Image.getWidth(), Image.getHeight()));
+        frame.setMinimumSize(new Dimension(Image.getWidth(), Image.getHeight()));
+      //  frame.setResizable(false);
+
+        frame.setVisible(true);
+    }
+
 
 
     public static void main(String[] args) {
 
-        ZipGPS zippy = new ZipGPS();
+        try {
+            ZipGPS zippy = new ZipGPS();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        System.out.println(zippy.convert(35, 102));
+
     }
 }
